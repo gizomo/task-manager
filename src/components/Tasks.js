80e03a2e5252cc43818730b8192cffcb6d72b36f
gizomo <gizomo@ya.ru>
2021-09-08 02:25:08 +0700

    added animations for tasks and alerts

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { AlertContext } from "../context/alert/alertContext";
 
 export const Tasks = ({ tasks, onRemove }) => {
@@ -10,22 +11,28 @@ export const Tasks = ({ tasks, onRemove }) => {
   }
 
   return (
-    <ul className="list-group">
+    <TransitionGroup component="ul" className="list-group">
       {tasks.map((task) => (
-        <li className="list-group-item task" key={task.id}>
-          <div>
-            <strong>{task.title}</strong>
-            <small>{new Date(task.date).toLocaleString()}</small>
-          </div>
-          <button
-            onClick={() => removeHadler(task.id)}
-            type="button"
-            className="btn btn-outline-danger btn-sm"
-          >
-            &times;
-          </button>
-        </li>
+        <CSSTransition
+          key={task.id}
+          classNames={'task'}
+          timeout={800}
+        >
+          <li className="list-group-item task">
+            <div>
+              <strong>{task.title}</strong>
+              <small>{new Date(task.date).toLocaleString()}</small>
+            </div>
+            <button
+              onClick={() => removeHadler(task.id)}
+              type="button"
+              className="btn btn-outline-danger btn-sm"
+            >
+              &times;
+            </button>
+          </li>
+        </CSSTransition>
       ))}
-    </ul>
+    </TransitionGroup>
   );
 };
